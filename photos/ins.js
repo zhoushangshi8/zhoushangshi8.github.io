192dff3e916dbbf6d400fae926dd8dabee4bf8ee
zhoushangshi8 <312979723@qq.com>
2021-07-10 09:21:20 +0800

    Site updated: 2021-07-10 09:21:07

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,10 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/photos/' + data.link[i];
+          //var minSrc = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/min_photos/' + data.link[i];
+		  //var src = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/photos/' + data.link[i];
+		  var minSrc = 'https://gitee.com/zhoushangshi8/blog-back-up/raw/master/min_photos/' + data.link[i];
+          var src = 'https://gitee.com/zhoushangshi8/blog-back-up/raw/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
